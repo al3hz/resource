2e36a37398680513cd80a8f5303cb96d914c5427
al3hz <dassault87@gmail.com>
2024-03-12 19:41:58 -0400

    Ajustes de texto

--- app/components/descripcion.tsx
+++ app/components/descripcion.tsx
@@ -1,7 +1,7 @@
 export default function Descripcion() {
     return (
       <div >
-        <p className="descripcion text-center mt-3 mb-1">Hi, Im overlain! || 20yo || Venezuelan || Sextrance enjoyer ||
+        <p className="descripcion text-center mt-3 mb-4">Hi, Im overlain! || 20yo || Venezuelan || Sextrance enjoyer ||
           Overlain#4697</p>
       </div>
     );
