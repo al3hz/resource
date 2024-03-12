7b4d6cf29b33b3241d7be2a35fd4167c3c4e987a
al3hz <dassault87@gmail.com>
2024-03-12 19:38:10 -0400

    punto y coma nojoda

--- app/components/descripcion.tsx
+++ app/components/descripcion.tsx
@@ -2,7 +2,7 @@ export default function Descripcion() {
     return (
       <div >
         <p className="descripcion text-center mt-3 mb-1">Hi, Im overlain! || 20yo || Venezuelan || Sextrance enjoyer ||
-          Overlain#4697</p>;
+          Overlain#4697</p>
       </div>
     );
   }
